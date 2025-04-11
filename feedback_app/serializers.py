b38acdcc4b468f7a7b4710799891bfa219245614
Vijay Reddy <vijayreddy6812@gmail.com>
2025-04-11 08:21:25 -0400

    Update serializers.py

--- feedback_app/serializers.py
+++ feedback_app/serializers.py
@@ -1,1 +1,13 @@
 
+from rest_framework import serializers
+from .models import Course, Feedback
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+class FeedbackSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Feedback
+        fields = '__all__'
